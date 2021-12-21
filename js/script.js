1adf335f7e52fb6e5277452d01fbdbfadd4b449d
Jeremy Gieske <jeremy.gieske@gmail.com>
2021-12-21 00:03:47 -0600

    improved timer and fixed check answer bug

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ let questionEl = document.querySelector("#questions")
 let answersEl = document.querySelector("#answers")
 let responseEl = document.querySelector("#response")
 let endGameEl = document.querySelector("#end-game")
+let formEl = document.querySelector("#high-score")
 let currentQuestion = 0
 
 //setup default time
@@ -19,27 +20,39 @@ const questionList = [
 {
     id: 0,
     question: "Commonly included data types do NOT include:",
-    possibleAnswer: {
-    a: "strings",
-    b: "booleans",
-    c: "alerts",
-    d: "numbers", 
-    },
-    correctAnswer: "c"
+    possibleAnswer: [
+    "strings",
+    "booleans",
+    "alerts",
+    "numbers" 
+],
+    correctAnswer: "alerts"
 },
 {
     id: 1,
-    question: "Question 2:",
-    possibleAnswer: {
-    a: "1: adsf",
-    b: "asdfgggg",
-    c: "asdghhhh",
-    d: "lkjojpo9ijg", 
-    },
-    correctAnswer: "a"
+    question: "question 2:",
+    possibleAnswer: [
+    "answer1",
+    "answer2",
+    "answer3",
+    "answer4" 
+],
+    correctAnswer: "answer1"
 },
-]
 
+{
+    id: 2,
+    question: "question 3:",
+    possibleAnswer: [
+    "apple",
+    "orange",
+    "banana",
+    "apricot" 
+],
+    correctAnswer: "apricot"
+},
+
+]
 
 // Start timer and remove initial instructions
 const begin = () => {
@@ -55,8 +68,9 @@ const begin = () => {
 
     let elem = document.querySelector("#start")
     elem.parentNode.removeChild(elem)
-
+ 
     quiz()
+    
 }
 
 
@@ -69,35 +83,31 @@ const setupQuiz = () => {
     //create the start button
     let instructionButton = document.createElement("button")
     instructionButton.textContent = "Start the Quiz"
-    instructionButton.className = "button"
+    instructionButton.className = "start-button"
     instructionsEl.appendChild(instructionButton)
 }
 
-
 //instantiate questions and answer buttons from the next available question
 const quiz = () => {
-    
-    //setup each question
-    console.log("Display a question")
-    if (currentQuestion < questionList.length) {
+
+    if (currentQuestion < questionList.length){
+
         questionEl.textContent = questionList[currentQuestion].question
 
-        //clear any previous existing buttons
-        while (answersEl.firstChild) {
-            answersEl.removeChild(answersEl.firstChild)
+            // clear any previous existing buttons
+                while (answersEl.firstChild) {
+                    answersEl.removeChild(answersEl.firstChild)
+                    }
+            //create answer list
+            const possibleAnswers = questionList[currentQuestion].possibleAnswer
+            for(var j = 0; j< possibleAnswers.length; j++){
+                let buttonEl = document.createElement("button");
+                buttonEl.textContent = possibleAnswers[j];
+                buttonEl.value = possibleAnswers[j];
+                buttonEl.className = "button";
+                answersEl.appendChild(buttonEl);
             }
-
-        for (const answer in questionList[currentQuestion].possibleAnswer) {
-
-            console.log(questionList[answer])
-
-            //create the answer buttons
-            let buttonEl = document.createElement("button");
-            buttonEl.textContent = answer;
-            buttonEl.className = "button";
-            answersEl.appendChild(buttonEl);
-        }      
-    } else {
+    }else {
         endGame()
     }
 }
@@ -106,17 +116,25 @@ const quiz = () => {
 const checkAnswer = (event) => {
     let targetEl = event.target.value
     let correct = questionList[currentQuestion].correctAnswer
-    console.log(targetEl)
-    console.log(correct)
 
     if (targetEl === correct) {
         responseEl.textContent = "Correct!"
         console.log("correct")
+        if (time <= 0){
+            time = 0
+            timerDisplay.textContent = `Time: ${time}`
+            endGame()
+        }
         currentQuestion++
     } else {
         responseEl.textContent = "Wrong!"
-        console.log("wrong")
+        // console.log("wrong")
         time = time - 10
+        if (time <= 0){
+            time = 0
+            timerDisplay.textContent = `Time: ${time}`
+            endGame()
+        }
         currentQuestion++
     }
     quiz()
@@ -125,8 +143,10 @@ const checkAnswer = (event) => {
 const endGame = () => {
 
     clearInterval(timer)
+    currentQuestion = 0
 
     // clear questions and answers but leave last response for context
+    
     while (questionEl.firstChild) {
         questionEl.removeChild(questionEl.firstChild)
     }
@@ -134,7 +154,9 @@ const endGame = () => {
         answersEl.removeChild(answersEl.firstChild)
     }
     //display end game message and score entry
-    endGameEl.textContent = `All Done! Your final score was ${time}`  
+    endGameEl.textContent = `All Done! Your final score was ${time}`
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    formEl.textContent = `Enter your initials:`  
 }
 
 const highScore = () => {
@@ -143,7 +165,8 @@ const highScore = () => {
         questionAnswers.removeChild(questionAnswers.firstChild)
     }
     //display high-score table
-    questionAnswers.textContent = `Below are the high-scores:`  
+    questionAnswers.textContent = `Below are the high-scores:`
+    
 
 }
 
@@ -154,8 +177,8 @@ const highScore = () => {
 setupQuiz()
 
 // event listeners
-document.querySelector("#start").addEventListener("click", e => {begin()})
-document.querySelector("#question-answers").addEventListener("click", checkAnswer)
+document.querySelector(".start-button").addEventListener("click", e => {begin()})
+document.querySelector("#answers").addEventListener("click", checkAnswer)
 document.querySelector("#high-score").addEventListener("click", highScore)
 
 
@@ -192,3 +215,29 @@ document.querySelector("#high-score").addEventListener("click", highScore)
 //     buttonEl.setAttribute("id", id);
 //     appendTo.appendChild(buttonEl);
 // }
+
+
+    // //setup each question
+    // console.log("Display a question")
+    // if (currentQuestion < questionList.length) {
+    //     questionEl.textContent = questionList[currentQuestion].question
+    //     // console.log("question:", questionList[currentQuestion].possibleAnswer.a)
+
+    //     //clear any previous existing buttons
+    //     while (answersEl.firstChild) {
+    //         answersEl.removeChild(answersEl.firstChild)
+    //         }
+
+    //     for (const answer in questionList[currentQuestion].possibleAnswer) {
+
+    //         console.log("answers buttons:", answer)
+
+    //         //create the answer buttons
+    //         let buttonEl = document.createElement("button");
+    //         buttonEl.textContent = answer;
+    //         buttonEl.className = "button";
+    //         answersEl.appendChild(buttonEl);
+    //     }      
+    // } else {
+    //     endGame()
+    // }
